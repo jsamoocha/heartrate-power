eb5b9171ca04bdf7d85596f2db37ac0f087326eb
Aart Goossens <aart@goossens.me>
2018-05-04 20:27:26 +0200

    Improving batch processing script

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
+import datetime
+import multiprocessing as mp
 import os
+import time
 
 import pandas as pd
 
@@ -10,33 +13,60 @@ ATHLETES = [
     'f1f70037993a7c837808717c9666cf4d6a730025b366b8734f00ef4319c8a12d',
 ]
 
-def batch_model_fit(athlete, output_file='model_fits.csv'):
-    model_fits = []
 
-    for filename in os.listdir(os.path.join('data', athlete)):
-        print(f'processing {filename}')
-        df = pd.read_json(
-            os.path.join('data', athlete, filename),
-            orient='split'
+def _process_single_file(filename):
+    df = pd.read_json(
+        os.path.join('data', _process_single_file.athlete, filename),
+        orient='split'
+    )
+
+    subdf = df[['heartrate', 'watts']].dropna(how='any')
+
+    try:
+        model, predictions = heartrate_model(
+            heartrate=subdf.heartrate,
+            power=subdf.watts
         )
+    except ValueError:
+        # Brute way to handle fitting errors
+        return
 
-        subdf = df[['heartrate', 'watts']].dropna(how='any')
-
-        try:
-            model, predictions = heartrate_model(
-                heartrate=subdf.heartrate,
-                power=subdf.watts
-            )
-        except ValueError:
-            # Brute way to handle fitting errors
-            continue
-
-        fit = {key: value.value for key, value in model.params.items()}
-        model_fits.append(fit)
+    fit = {key: value.value for key, value in model.params.items()}
+    fit['athlete_id'] = _process_single_file.athlete
+    fit['activity_id'] = filename.split('.')[0]
+    
+    _process_single_file.queue.put(fit)
 
 
+def _func_init(queue, athlete):
+    _process_single_file.queue = queue
+    _process_single_file.athlete = athlete
+
+
+def batch_model_fit(athlete):
+    start_time = time.time()
+
+    files_for_athlete = os.listdir(os.path.join('data', athlete))
+    queue = mp.Queue()
+    with mp.Pool(mp.cpu_count(), _func_init, [queue, athlete]) as pool:
+        pool.map(_process_single_file, files_for_athlete, chunksize=10)
+
+    model_fits = []
+    while not queue.empty():
+        model_fits.append(queue.get())
     df = pd.DataFrame(model_fits)
-    df.to_csv(output_file)
+    df.to_csv(datetime.datetime.now().isoformat() + '.csv')
+
+    duration = time.time() - start_time
+    if duration >= 60:
+        seconds = int(round(duration % 60))
+        if seconds < 10:
+            seconds = '0' + str(seconds)
+        minutes = int(round(duration/60, 0))
+        duration_string = f'{minutes}:{seconds} (mm:ss)'
+    else:
+        duration_string = f'{int(round(duration, 0))} seconds'
+    print(f'Processing took: {duration_string}')
 
 
 if __name__ == "__main__":
